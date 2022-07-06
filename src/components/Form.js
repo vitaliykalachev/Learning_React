e16862957c2d0afcbac717d3c6b2a217b98a8726
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-07-06 10:37:17 +0700

    Lifecycle Methods, Fetch data from Api

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,7 +9,15 @@ export class Form extends Component {
       this.state = {
          username: '',
          password: '',
+         posts:[]
       }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => this.setState({posts: data}))
+
     }
 
     usernameHandler = (event) => {
@@ -26,11 +34,16 @@ export class Form extends Component {
     }
   
     render() {
+    const {posts} = this.state
     return (
       <div className="container">
           <input type="text" value={this.state.username} placeholder="Enter your username" className = "form-control" onChange={this.usernameHandler}/>
           <input type="password" value={this.state.password} placeholder="Enter your Password" className = "form-control" onChange={this.passwordHandler}/>
           <button className="btn btn-primary">Click me</button>
+      
+            {posts.map(post => 
+            <h2 key = {post.id}>{post.title}</h2>
+            )}
       </div>
     )
   }
